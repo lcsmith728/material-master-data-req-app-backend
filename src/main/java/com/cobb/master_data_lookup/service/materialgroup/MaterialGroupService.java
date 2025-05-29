bb37d2b22aaeef03d572f9a908b36b1b48755df1
lcsmith728 <lcsmith72818@gmail.com>
2025-05-29 16:47:59 -0500

    Initial Setup connecting to local db

--- src/main/java/com/cobb/master_data_lookup/service/materialgroup/MaterialGroupService.java
+++ src/main/java/com/cobb/master_data_lookup/service/materialgroup/MaterialGroupService.java
@@ -11,7 +11,7 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 
 @Service
-@@RequiredArgsConstructor
+@RequiredArgsConstructor
 public class MaterialGroupService implements IMaterialGroupService {
 
     private final MaterialGroupRepository materialGroupRepository;
